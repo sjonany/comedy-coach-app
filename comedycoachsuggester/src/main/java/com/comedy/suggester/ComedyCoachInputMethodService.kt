95442da3841c582f1a747735b1ca9e500c59df59
Stephen Jonany <sjonany@gmail.com>
2024-12-27 17:37:38 -0800

    Replaced keyboard w/ hardcoded suggestions + replacement

--- comedycoachsuggester/src/main/java/com/comedy/suggester/ComedyCoachInputMethodService.kt
+++ comedycoachsuggester/src/main/java/com/comedy/suggester/ComedyCoachInputMethodService.kt
@@ -1,60 +1,38 @@
 package com.comedy.suggester
 
 import android.inputmethodservice.InputMethodService
-import android.inputmethodservice.Keyboard
-import android.inputmethodservice.KeyboardView
 import android.view.View
-import com.comedy.suggester.ui.keyboard.CustomKeyboardView
+import android.widget.Button
 
-class ComedyCoachInputMethodService : InputMethodService(), KeyboardView.OnKeyboardActionListener {
+private const val ALL_CHARACTERS = 1000
 
-    private lateinit var keyboard: Keyboard
-    private lateinit var keyboardView: CustomKeyboardView
-
-
-    override fun onCreate() {
-        super.onCreate()
-        keyboard = Keyboard(this, R.xml.qwerty_keyboard)
-    }
+class ComedyCoachInputMethodService : InputMethodService() {
 
     override fun onCreateInputView(): View {
-        keyboardView = layoutInflater.inflate(R.layout.keyboard, null) as CustomKeyboardView
-        keyboardView.keyboard = keyboard
-        keyboardView.setOnKeyboardActionListener(this)
-        return keyboardView
-    }
-    
-    // Implement required KeyboardView.OnKeyboardActionListener methods
-    override fun onKey(primaryCode: Int, keyCodes: IntArray?) {
-        // Handle key press
-        currentInputConnection.commitText(primaryCode.toChar().toString(), 1)
+        // Inflate the custom view with buttons or options
+        val inputView = layoutInflater.inflate(R.layout.suggestions, null)
+
+        // Set listeners for the buttons
+        val option1 = inputView.findViewById<Button>(R.id.option1)
+        val option2 = inputView.findViewById<Button>(R.id.option2)
+        val option3 = inputView.findViewById<Button>(R.id.option3)
+        val finishButton = inputView.findViewById<Button>(R.id.finish)
+
+        option1.setOnClickListener { replaceText("Option 1 Selected") }
+        option2.setOnClickListener { replaceText("Option 2 Selected") }
+        option3.setOnClickListener { replaceText("Option 3 Selected") }
+        finishButton.setOnClickListener {
+            // Close the custom IME, forcing Android to switch to the default IME
+            requestHideSelf(0)  // Pass 0 for the flags to hide the IME
+        }
+
+        return inputView
     }
 
-    override fun onPress(p0: Int) {
-        TODO("Not yet implemented")
-    }
-
-    override fun onRelease(p0: Int) {
-        TODO("Not yet implemented")
-    }
-
-    override fun onText(p0: CharSequence?) {
-        TODO("Not yet implemented")
-    }
-
-    override fun swipeLeft() {
-        TODO("Not yet implemented")
-    }
-
-    override fun swipeRight() {
-        TODO("Not yet implemented")
-    }
-
-    override fun swipeDown() {
-        TODO("Not yet implemented")
-    }
-
-    override fun swipeUp() {
-        TODO("Not yet implemented")
+    private fun replaceText(text: String) {
+        val currentInputConnection = currentInputConnection
+        // Clear the text input, then append
+        currentInputConnection.deleteSurroundingText(ALL_CHARACTERS, ALL_CHARACTERS)
+        currentInputConnection?.commitText(text, 1)
     }
 }
