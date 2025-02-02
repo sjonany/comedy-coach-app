00ce31051ed547fee8480bfaca30aa3276b1abf3
Stephen Jonany <sjonany@gmail.com>
2025-02-02 09:19:50 +0700

    Prettified reset humor button

--- comedycoachsuggester/src/main/java/com/comedy/suggester/ui/charactereditor/HumorSettingScreen.kt
+++ comedycoachsuggester/src/main/java/com/comedy/suggester/ui/charactereditor/HumorSettingScreen.kt
@@ -1,10 +1,12 @@
 package com.comedy.suggester.ui.charactereditor
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -17,11 +19,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.MutableCreationExtras
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import com.comedy.suggester.R
 import com.comedy.suggester.SuggesterApplication
 import com.comedy.suggester.data.CharacterProfile
 import com.comedy.suggester.generator.PromptStrings
@@ -93,25 +97,35 @@ fun HumorEditorWidget(
                 .height(600.dp)
                 .padding(bottom = 8.dp)
         )
-        Button(
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            onClick = {
-                newSenseOfHumor = PromptStrings.DEFAULT_SENSE_OF_HUMOR
-            }) {
-            Text("Use default sense of humor")
-        }
-        Button(
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            onClick = {
-                onSave(
-                    CharacterProfile(
-                        characterProfile.id,
-                        newSenseOfHumor,
-                        characterProfile.aliases
+
+        Row(
+            modifier = Modifier.padding(bottom = 8.dp),
+        ) {
+            Button(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(8.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = colorResource(id = R.color.cancel)),
+                onClick = {
+                    newSenseOfHumor = PromptStrings.DEFAULT_SENSE_OF_HUMOR
+                }) {
+                Text("Reset")
+            }
+            Button(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(8.dp),
+                onClick = {
+                    onSave(
+                        CharacterProfile(
+                            characterProfile.id,
+                            newSenseOfHumor,
+                            characterProfile.aliases
+                        )
                     )
-                )
-            }) {
-            Text("Save")
+                }) {
+                Text("Save")
+            }
         }
     }
 }
